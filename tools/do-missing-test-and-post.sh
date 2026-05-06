cf4a583d9ec03da03b02148ba9fa24ecf552b3c3
Tim Bird <tim.bird@sony.com>
2026-05-06 14:55:01 -0600

    Add -r option to do-missing-test-and-post.sh
    Allow the user to remove the results file from their
    local system, as needed.
    
    Signed-off-by: Tim Bird <tim.bird@sony.com>

--- tools/do-missing-test-and-post.sh
+++ tools/do-missing-test-and-post.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # do-missing-test-and-post.sh
 #
-## support -i for specifing the input file (results), instead of
-# running the test
-#
+
+# uncomment the next line to debug
+#set -x
+
 if [ "$1" = "-h" -o "$1" = "--help" ] ; then
     cat <<USAGE
 Usage: do-missing-test-and-post.sh [options]
@@ -11,9 +12,12 @@ Usage: do-missing-test-and-post.sh [options]
 Perform get-kernel-spdx-missing-list-top-dirs.sh, and post it
 to Tim Bird's boot-time wiki.
 
+!! Note: This script needs 'tbclient' to upload the page to the wiki.
+
 Options:
  -h or --help   Show this usage help
  -i <infile>    Use <infile> instead of running the test
+ -r             Remove file after posting it to the wiki
 USAGE
     exit 0
 fi
@@ -32,6 +36,11 @@ else
     ./get-kernel-spdx-missing-list-top-dirs.sh -b | tee $RESULTS_FILE
 fi
 
+if [ "$1" = "-r" ] ; then
+    shift
+    REMOVE_FILE=1
+fi
+
 TIMESTAMP="$(date +%Y-%m-%d_%H:%M:%S)"
 PAGE_NAME="Kernel_Dirs_Missing_SPDX_Counts_$TIMESTAMP"
 
@@ -39,6 +48,11 @@ PAGE_NAME="Kernel_Dirs_Missing_SPDX_Counts_$TIMESTAMP"
 # has colons
 echo "Putting data to boot-time wiki..."
 tbclient -w birdcloud.org:/bc put_page $PAGE_NAME --infile $RESULTS_FILE
+
+if [ -n "$REMOVE_FILE" ] ; then
+    echo "Removing results file..."
+    rm $RESULTS_FILE
+fi
 
 echo "  You should be able to access the test results at:"
 echo "     https://birdcloud.org/bc/$PAGE_NAME"
